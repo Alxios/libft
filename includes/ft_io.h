1fdb2cec6f338180b8e8ca51818aeb2ed52e4600
agaspar <agaspar@student.42.fr>
2016-03-23 00:35:42 +0100

    Refractoring END

--- includes/ft_io.h
+++ includes/ft_io.h
@@ -15,15 +15,16 @@
 
 # include "ft_printf.h"
 # include "get_next_line.h"
+
 # include <unistd.h>
 # include <wchar.h>
 
 void	ft_putchar(char c);
 void	ft_putstr(char const *s);
-void  ft_putnstr(char const *s, int const len);
+void	ft_putnstr(char const *s, int const len);
 void	ft_putendl(char const *s);
 void	ft_putcol(const char *s, unsigned int color);
-void  ft_putwchar(wchar_t c);
+void	ft_putwchar(wchar_t c);
 void	ft_putwstr(wchar_t *str);
 void	ft_putnwstr(wchar_t *str, int length);
 
@@ -32,10 +33,10 @@ void	ft_putnbr_ndl(int i);
 
 void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char const *s, int fd);
-void  ft_putnstr_fd(char const *s, int const len, int fd);
+void	ft_putnstr_fd(char const *s, int const len, int fd);
 void	ft_putendl_fd(char const *s, int fd);
 void	ft_putcol_fd(const char *s, unsigned int color, int fd);
-void  ft_putwchar_fd(wchar_t c, int fd);
+void	ft_putwchar_fd(wchar_t c, int fd);
 void	ft_putwstr_fd(wchar_t *str, int fd);
 void	ft_putnwstr_fd(wchar_t *str, int length, int fd);
 
